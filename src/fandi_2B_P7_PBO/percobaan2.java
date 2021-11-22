15449e3d324f094b2999c7f8d9e350e520e2b1ef
Affandi Pratama <60037959+afanditama12@users.noreply.github.com>
2021-11-22 20:21:49 +0700

    fix error di class percobaan 2

--- src/fandi_2B_P7_PBO/percobaan2.java
+++ src/fandi_2B_P7_PBO/percobaan2.java
@@ -5,7 +5,11 @@ public class percobaan2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int angka = 15;
-		System.out.println(angka/0);
+		try {			
+			System.out.println(angka/0);
+		} catch(ArithmeticException obj) {
+			System.out.println("Tidak boleh membagi bilangan dengan 0");
+		}
 	}
 
 }
